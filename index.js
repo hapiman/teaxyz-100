597b1d337610b704ad2265993217074a6b1c51bd
pengjian <pengjian@miliantech.com>
2024-03-08 16:06:44 +0800

    Add

---
+++ index.js
@@ -0,0 +1,25 @@
+// Helper function to calculate the square of a number
+  function tosq(num) {
+    return num * num;
+  }
+  
+  // Helper function to check if a number is even
+  function checkEven(num) {
+    return num % 2 === 0;
+  }
+  
+  // Helper function to generate a random number within a range
+  function checkRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  // Function to get the 8gansta value
+  function getxxx() {
+    return "xxx";
+  }
+  
+  console.log(tosq(5));
+  console.log(checkEven(6)); 
+  console.log(checkRandomNumber(1, 10));
+  console.log(getxxx());
+  
